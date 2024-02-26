9c017bcda3da7e25d15e9219a7d2f5e321157b3a
wahyukusuma <wahyukusumawardhana17@gmail.com>
2024-02-27 01:30:52 +0700

    first commit

---
+++ src/LibrarySystem.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class LibrarySystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String adminUsername = "Admin";
+        String adminPassword = "admin";
+
+        System.out.println("Library System\n");
+
+        int option = 0;
+        while (option != 3) {
+            System.out.println("1. Login as Student");
+            System.out.println("2. Login as Admin");
+            System.out.println("3. Exit\n");
+            System.out.print("Choose option (1-3): ");
+            option = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (option) {
+                case 1:
+                    System.out.print("\nEnter your NIM: ");
+                    String NIM = scanner.nextLine();
+                    if (NIM.length() == 15) {
+                        System.out.println("Successful Login as Student\n");
+                    } else {
+                        System.out.println("User Not Found\n");
+                    }
+                    break;
+                case 2:
+                    System.out.print("\nEnter your username(admin): ");
+                    String username = scanner.nextLine();
+                    System.out.print("Enter your password(admin): ");
+                    String password = scanner.nextLine();
+
+                    if (username.equals(adminUsername) && password.equals(adminPassword)) {
+                        System.out.println("\nSuccessful Login as Admin\n");
+                    } else {
+                        System.out.println("\nUser Not Found!!\n");
+                    }
+                    break;
+                case 3:
+                    System.out.println("\nadios");
+                    break;
+                default:
+                    System.out.println("\nInvalid option\n");
+                    break;
+            }
+        }
+        scanner.close();
+    }
+}
